87c77144fc2d37a9d6bd1879473404c15deec9ed
Angelika Brown <angelikpbrown@gmail.com>
2025-02-28 11:33:03 -0500

    Something is here

---
+++ helloWorld.java
@@ -0,0 +1,11 @@
+System.out.println("Hello, World!")
+int x =5;
+x
+x + 5
+x
+x = x + 10
+x
+ System.out.println("Welcome");
+System.out.println("Welcome");
+System.out.print("Angelika");
+System.out.print("Angelika");
