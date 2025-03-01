ae33fb68b2f125a6b828b75a480378d1fd600cb2
Angelika Brown <angelikpbrown@gmail.com>
2025-02-28 21:49:35 -0500

    done with jshell and JavaSmall

---
+++ oddEvenMethods.java
@@ -0,0 +1,14 @@
+var x = 5;
+var y = 2;
+x % y
+int i = 1;
+while (i <= 10) {
+    System.out.println(i + " % 2 = " + i % 2);
+    i++;
+}
+boolean isEven(int x) {
+    return x % 2 == 0;
+}
+boolean isOdd(int x) {
+    return x % 2 != 0;
+}
---
+++ printOddEven.java
@@ -0,0 +1,10 @@
+void printOddEven() {
+    int i = 1;
+    while (i <= 10) {
+        if (isEven(i)) {
+            System.out.println(i + " is even");
+        } else {
+            System.out.println(i + " is odd");
+        }
+        i++;
+    }}
