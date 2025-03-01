29d4c8e3efc724f80dc16ad9545d61ce6de1b918
Angelika Brown <angelikpbrown@gmail.com>
2025-02-28 21:10:32 -0500

    Math methods done

---
+++ mathMethods.java
@@ -0,0 +1,13 @@
+int addTwoNumber(int x, int y){
+    return x + y;
+}
+addTwoNumber(5, 10);
+int divideTwoNumbers(int x, int y) {
+    return x / y;
+}
+int multiplyTwoNumbers(int x, int y){
+    return x * y;
+}
+int subtractTwoNumbers(int x, int y){
+    return x - y;
+}
